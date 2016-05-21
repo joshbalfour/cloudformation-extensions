e779bb8f1b3f54142daa603f5c14852d671228f9
Josh Balfour <josh@joshbalfour.co.uk>
2016-05-21 18:06:16 +0100

    started loading extensions

--- cloudformation-extensions.js
+++ cloudformation-extensions.js
@@ -2,7 +2,7 @@
 
 const path = require('path');
 const { readJSON, args } = require('./lib/utils');
-const { getCfnexStatements } = require('./lib/cfnex');
+const { getCfnexStatements, loadExtensions, processStatement } = require('./lib/cfnex');
 
 let filePath = path.resolve('.', args[0]);
 
@@ -14,12 +14,22 @@ readJSON(filePath)
 		console.log('Parsed cfn JSON file successfully');
 		console.log(`This stack's description is: "${cfn.Description}"`);
 		
-		return getCfnexStatements(cfn);
+		return Promise.all([ 
+			getCfnexStatements(cfn), 
+			loadExtensions()
+		]);
 
 	})
-	.then(function(statements){
+	.then(function([statements, extensions]){
 		console.log(`Found ${statements.length} cfnex statements to process`);
-		console.log(statements);
+
+		return Promise.all(
+			statements.map( statement => processStatement(statement, extensions) )
+		);
+
+	})
+	.then(function(processedStatement){
+		console.log('Processed statements:', processedStatement);
 	})
 	.catch(function(err){
 		console.error('Error:',err);
