26477ae8cb91684124af82b5244f2f90b39e72ff
Josh Balfour <josh@joshbalfour.co.uk>
2016-05-22 20:44:13 +0100

    rewrote main core

--- cloudformation-extensions.js
+++ cloudformation-extensions.js
@@ -1,42 +1,33 @@
 'use strict';
 
 const path = require('path');
+
 const { readJSON, args } = require('./lib/utils');
-const { getCfnexStatements, loadExtensions, processStatement } = require('./lib/cfnex');
+const { parseStatement, loadExtensions, processStatement, parse } = require('./lib/cfnex');
 
 let filePath = path.resolve('.', args[0]);
 
 console.log(`Reading in file: ${filePath}`);
 
-readJSON(filePath)
-	.then(function(cfn){
-		
+Promise.all([ 
+		readJSON(filePath), 
+		loadExtensions()
+	])
+	.then(function([cfn, extensions]){
 		console.log('Parsed cfn JSON file successfully');
 		console.log(`This stack's description is: "${cfn.Description}"`);
-		
-		return Promise.all([ 
-			getCfnexStatements(cfn), 
-			loadExtensions()
-		]);
-
-	})
-	.then(function([statements, extensions]){
 		console.log(`Loaded ${extensions.length} extensions`);
-		console.log(`Found ${statements.length} cfnex statements to process`);
-		
-		let context = {
-			srcFileName: filePath
-		};
 
-		return Promise.all(
-			statements.map( statement => processStatement(statement, extensions, context) )
-		);
-
-	})
-	.then(function(processedStatements){
-		processedStatements.forEach(function(processedStatement){
-			console.log(processedStatement);
+		return parse({
+				obj: cfn, 
+				context: { srcFileName: filePath, extensions: extensions }
+			}).then(function(){
+			return cfn;
 		});
+	})
+	.then(function(cfn){
+		console.log("Completed!");
+		console.log(cfn);
 	})
 	.catch(function(err){
 		console.error('Error:', err);
