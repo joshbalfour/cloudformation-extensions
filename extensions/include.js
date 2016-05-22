ff571066f2d07fbce787bb8125c541707cd418be
Josh Balfour <josh@joshbalfour.co.uk>
2016-05-22 21:34:41 +0100

    extension: update include to allow inclusion of .js files

--- extensions/include.js
+++ extensions/include.js
@@ -2,17 +2,30 @@ const fs = require('fs'),
 	  path = require('path'),
 	  promisify = require("es6-promisify");
 
-const { readJSON } = require('../lib/utils');
+const { readJSON, readFile } = require('../lib/utils');
 
 function include([filename], { srcFileName }){
 	let fn = path.resolve(srcFileName, '..', filename);
-	console.log(`including file ${fn}`);
-	return readJSON(fn).then(function(output){
-		return {
-			output,
-			contextChanges: { srcFileName: fn }
-		};
-	});
+	console.log(`including JSON/JS file ${fn}`);
+
+	if (path.extname(fn) == '.json'){
+		return readJSON(fn).then(function(output){
+			return {
+				output,
+				contextChanges: { srcFileName: fn }
+			};
+		});
+	} else if (path.extname(fn) == '.js') {
+		let file = promisify(new require(fn));
+		return file().then(function(output){
+			return {
+				output: output,
+				contextChanges: { srcFileName: fn }
+			};
+		});
+	} else {
+		throw new Error("Include can only be used for JSON files and JS files which module.export an object");
+	}
 }
 
 module.exports = include;
