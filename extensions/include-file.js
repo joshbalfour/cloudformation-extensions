6d22a81cdf512c2f2cc6fe236278682c1926c776
Josh Balfour <josh@joshbalfour.co.uk>
2016-05-24 10:46:56 +0100

    include raw files with substitution strings

---
+++ extensions/include-file.js
@@ -0,0 +1,31 @@
+const fs = require('fs'),
+	  path = require('path');
+
+const { readFile } = require('../lib/utils');
+
+function replaceSubs(str){
+	if (str.indexOf('<%cfnex') !== -1 && str.indexOf('cfnex%>') !== -1){
+		let left = str.split('<%cfnex')[0];
+		let middle = str.split('<%cfnex')[1].split('cfnex%>')[0];
+		let right = str.split('cfnex%>')[1];
+		return { "Fn::Join": ["", [left, middle, right]] };
+	}
+	return str;
+}
+
+function includeFile(args, { srcFileName }){
+	let aargs = [... args ];
+	let filename = aargs.shift();
+	
+	let fn = path.resolve(srcFileName, '..', filename);
+	console.log(`including file ${fn}`);
+
+	return readFile(fn, 'utf-8')
+		.then(function(fileContents){
+			let split = fileContents.split('\n').map(replaceSubs);
+			let output = { "Fn::Join": ["\n", split] };
+			return {output};
+		});
+}
+
+module.exports = includeFile;
