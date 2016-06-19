dca29974a12396f3ef68445c79c1a5681a1e8e7c
Josh Balfour <josh@joshbalfour.co.uk>
2016-06-19 15:57:47 +0100

    Cloudformation Extensions/index.js tests

--- test/index.js
+++ test/index.js
@@ -1,4 +1,6 @@
-const assert = require('chai').assert;
+const assert = require('chai').assert,
+	  type = require('type-detect'),
+	  path = require('path');
 
 describe('Cloudformation Extensions', function(){
 
@@ -22,9 +24,96 @@ describe('Cloudformation Extensions', function(){
 		it('should exist', function(){
 			assert.isDefined(cloudformationExtensions.parseCfnex);
 			parseCfnex = cloudformationExtensions.parseCfnex;
+
 		});
 
-		
+		it('should return a promise', function(){
+			let context = {
+				cwd: '.',
+				config: {
+					extensionsDirectory: 'extensions'
+				}
+			};
+			
+			let returned = parseCfnex({}, context);
+
+			assert(type(returned) === 'promise');
+		});
+
+		it('should run end to end as intended', function(done){
+			let context = {
+				cwd: path.resolve(__dirname, '..', 'example'),
+				config: {
+					extensionsDirectory: '../extensions'
+				}
+			};
+
+			let cfnexObj = {
+				"AWSTemplateFormatVersion" : "2010-09-09",
+  				"Description" : "projectname Stack",
+  				"Mappings" : {
+  					"AWSRegionToAMI": {
+  						"cfnex::include" : [ "./amis.js" ]
+  					}
+  				}
+			};
+
+			let expectedCfn = {
+				"AWSTemplateFormatVersion" : "2010-09-09",
+  				"Description" : "projectname Stack",
+  				"Mappings" : {
+  					"AWSRegionToAMI": {
+						"us-east-1"      : { "AMIID" : "ami-67a3a90d" },
+						"us-west-1"      : { "AMIID" : "ami-b7d5a8d7" },
+						"us-west-2"      : { "AMIID" : "ami-c7a451a7" },
+						"eu-west-1"      : { "AMIID" : "ami-9c9819ef" },
+						"eu-central-1"   : { "AMIID" : "ami-9aeb0af5" },
+						"ap-northeast-1" : { "AMIID" : "ami-7e4a5b10" },
+						"ap-southeast-1" : { "AMIID" : "ami-be63a9dd" },
+						"ap-southeast-2" : { "AMIID" : "ami-b8cbe8db" }
+  					}
+  				}
+			};
+			
+			let returned = parseCfnex(cfnexObj, context);
+
+			returned.then(function(cfn){
+				assert.deepEqual(cfn, expectedCfn, 'cfnex generates the expected cfn object');
+				done();
+			}).catch(function(err){
+				throw err;
+			});
+
+		});
+
+		it('should error as intended', function(done){
+			let context = {
+				cwd: path.resolve(__dirname),
+				config: {
+					extensionsDirectory: '../extensions'
+				}
+			};
+
+			let cfnexObj = {
+				"AWSTemplateFormatVersion" : "2010-09-09",
+				"Description" : "projectname Stack",
+				"Mappings" : {
+					"AWSRegionToAMI": {
+						"cfnex::include" : [ "./amis.js" ]
+					}
+				}
+			};
+
+			let returned = parseCfnex(cfnexObj, context);
+
+			returned.then(function(cfn){
+				
+			}).catch(function(err){
+				assert.isDefined(err);
+				done();
+			});
+
+		});
 
 	});
 
