30fa82d56bbfbb6f9eb598721a25a69d31d189d4
Josh Balfour <josh@joshbalfour.co.uk>
2016-07-31 16:47:28 +0100

    tests

--- test/lib/cfnex.js
+++ test/lib/cfnex.js
@@ -1,8 +1,16 @@
-const assert = require('chai').assert;
+const chai = require('chai');
+const chaiAsPromised = require("chai-as-promised");
+chai.use(chaiAsPromised);
+const assert = chai.assert;
 
 describe('Cloudformation Extensions.cfnex', function(){
 	
 	let cfnex;
+
+	const logger = {
+		debug: console.log,
+		error: console.log
+	};
 
 	it('should init fine', function(){
 		cfnex = require('../../lib/cfnex');
@@ -17,15 +25,46 @@ describe('Cloudformation Extensions.cfnex', function(){
 	});
 
 	describe('loadExtensions', function(){
-		
+
+		it('should load the given extensions', function(){
+			const context = {
+				logger: logger,
+				cwd: '.',
+				config: {
+					extensionsDirectory: "extensions"
+				}
+			};
+			assert.isFulfilled(cfnex.loadExtensions(context));
+		});
+
+		it('should error if it can\'t find the given extensions directory', function(){
+			const context = {
+				logger: logger,
+				cwd: '..',
+				config: {
+					extensionsDirectory: "extensions"
+				}
+			};
+			assert.isRejected(cfnex.loadExtensions(context));
+		});
 	});
 
 	describe('parse', function(){
-		
+		it('should parse the given object', function(){
+
+		});
+		it('should error if given an unparsable object', function(){
+			
+		});
 	});
 
 	describe('parseStatement', function(){
-		
+		it('should parse the given statement', function(){
+
+		});
+		it('should error if given an unparsable statement', function(){
+			
+		});
 	});
 
 });
