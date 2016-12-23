1c963eebce4d7e9ceef6662145efe1c3acb806f4
Andy Neville <andy@secureinnovation.com>
2016-12-23 10:35:15 -0800

    When encountering <%cfnex tags in the include-file extension, this now attempts to parse the contents of the tag as JSON so that the outer template can use object references, etc.

--- extensions/include-file.js
+++ extensions/include-file.js
@@ -5,8 +5,12 @@ const { readFile } = require('../lib/utils')
 function replaceSubs(str) {
 	if (str.indexOf('<%cfnex') !== -1 && str.indexOf('cfnex%>') !== -1) {
 		const left = str.split('<%cfnex')[0]
-		const middle = str.split('<%cfnex')[1].split('cfnex%>')[0]
+		let middle = str.split('<%cfnex')[1].split('cfnex%>')[0]
 		const right = str.split('cfnex%>')[1]
+
+		try {
+			middle = JSON.parse(middle)
+		} catch (e) { /* if the parse fails, it will leave the original string intact */ }
 
 		return { 'Fn::Join': ['', [left, middle, right]] }
 	}
